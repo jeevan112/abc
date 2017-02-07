0c84d2740fefd444df5cf902a77480e2c3395aa6
G_one <G_one@DESKTOP-4IT2MMK>
2017-02-07 17:16:21 +0530

    last commit of tuesday

--- first proj/src/first_class.java
+++ first proj/src/first_class.java
@@ -4,7 +4,10 @@ public class first_class
 	public static void main(String args[])
 	{
 		System.out.println("Hello");
-		System.out.println("first commit");
+		System.out.println("first commit1");
+		System.out.println("Second commit1");
+		System.out.println("today i have learnt how to work with github");
+		
 		
 	}
 	
