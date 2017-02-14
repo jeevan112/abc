ee0dd47326cd411c0aef02b713a09ab7fc49d0ee
jeevan112 <jeevanbhatt7865@gmail.com>
2017-02-14 16:13:38 +0530

    Update first_class.java
    about github

--- first proj/src/first_class.java
+++ first proj/src/first_class.java
@@ -7,9 +7,8 @@ public class first_class
 		System.out.println("first commit1");
 		System.out.println("Second commit1");
 		System.out.println("today i have  vgflearnt how to work with github");
-		
-		
-	}
+		system.out.println("github is interesting i am loving that!!");
+		System.out.println("some problem are occuring as i am new to this");
 	
 
 
